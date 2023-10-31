50d4a66919ab34337d8cbacff658f72e2a7b6fb5
Lenard <lenardpiir@gmail.com>
2023-10-31 15:46:32 +0200

    initial api and frontend

---
+++ sectorapi/src/main/java/com/helmes/sectorapi/SectorApiApplication.java
@@ -0,0 +1,13 @@
+package com.helmes.sectorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectorApiApplication.class, args);
+	}
+
+}
---
+++ sectorapi/src/test/java/com/helmes/sectorapi/SectorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helmes.sectorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
