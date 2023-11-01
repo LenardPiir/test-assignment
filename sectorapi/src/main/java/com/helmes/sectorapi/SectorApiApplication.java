445fe2e9d45cea81a37b7378fd06bf4b3e37c998
Lenard <lenardpiir@gmail.com>
2023-11-01 11:19:13 +0200

    enabled transaction management

--- sectorapi/src/main/java/com/helmes/sectorapi/SectorApiApplication.java
+++ sectorapi/src/main/java/com/helmes/sectorapi/SectorApiApplication.java
@@ -2,8 +2,10 @@ package com.helmes.sectorapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SectorApiApplication {
 
 	public static void main(String[] args) {
