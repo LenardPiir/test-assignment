6098749b105ab155623411355167a5ff2e7deeb5
Lenard <lenardpiir@gmail.com>
2023-11-01 15:01:27 +0200

    added entities

---
+++ sectorapi/src/main/java/com/helmes/sectorapi/entities/IndustrySpecification.java
@@ -0,0 +1,17 @@
+package com.helmes.sectorapi.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class IndustrySpecification {
+
+    @Id
+    private Long id;
+}
---
+++ sectorapi/src/main/java/com/helmes/sectorapi/entities/Sector.java
@@ -0,0 +1,23 @@
+package com.helmes.sectorapi.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sector {
+
+    @Id
+    private Long id;
+
+    @OneToMany
+    private List<Industry> industries;
+}
